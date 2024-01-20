f1cd9bdb50a81dc169e49856f649a0f87bb11217
AgayevAmin <aghayevamin@gmail.com>
2024-01-20 14:18:55 +0400

    3

---
+++ js/main.js
@@ -0,0 +1,60 @@
+////////////////////////////////15//////////////////////////////
+//Check whether a given positive number is a multiple of 3 or a multiple of 7
+
+// function papa(x) 
+// {
+//   if (x % 3 == 0 || x % 7 == 0) 
+//   {
+//     return true;
+//   } 
+//   else {
+//     return false;
+//   }
+// }
+
+// console.log(papa(15));
+// console.log(papa(13));
+// console.log(papa(21));
+// console.log(papa(45));
+
+
+
+
+/////////////////////////////////16//////////////////////////////
+//Check whether two given integer values are in the range 50..99
+// function heci(x, y) 
+//   {
+//   if ((x >= 50 && x <= 99) || (y >= 10 && y <= 99)){
+//     return true;
+//     console.log("true")
+//   } 
+//   else {
+//     return false;
+//     console.log("false")
+//   }
+// }
+
+// console.log(heci(12, 101));
+// console.log(heci(42, 80));
+// console.log(heci(15, 59));
+// console.log(heci(15, 1009));
+// console.log(heci(0, 59999));
+
+/////////////////////////////////17///////////////////////
+//Removes non-printable ASCII characters from a given string
+// const heri = str => str.replace(/[^\x20-\x7E]/g, '');
+// console.log(heri('äÄçÇéÉêw3resou4545454545röce///////////defn835679346%%&^(*^(&^%&##$^&^#$#$^@^$*^#$@^$#%&UI&%*$%#@#&*)(&%@#@^$^(*@%&#$^*ÖÐþúÚ'));
+
+
+
+
+///////////////////////////////////18//////////////////
+////birinci herfi boyut 
+
+// var arr=["aaaaaaaaaa","ssssssssssss","dddddddddddd"]
+// var newarry=[]
+// arr.forEach(element =>{
+//     let a=element.charAt(0).toLocaleUpperCase()+element.slice(1)
+//     newarry.push(a)
+// } ) 
+// console.log(newarry)
